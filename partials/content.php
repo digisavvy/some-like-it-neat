60712cda245c72aa68663582990918424bb3abd3
Alex Vasquez <alexjvas@gmail.com>
2013-12-08 18:13:21 -0800

    adding genericons for index loop and post meta

--- partials/content.php
+++ partials/content.php
@@ -10,7 +10,7 @@
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php digistarter_posted_on(); ?>
+			<span class="genericon genericon-time"></span> <?php digistarter_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
