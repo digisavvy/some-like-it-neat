788d34f3aead16b26a9dd5fc0729a9f0e96c598c
Alex Vasquez <alexjvas@gmail.com>
2013-12-18 23:16:15 -0800

    formatting

--- functions.php
+++ functions.php
@@ -227,14 +227,14 @@ function neat_recommended_required_plugins() {
 
 		// This is an example of how to include a plugin pre-packaged with a theme
 		array(
-			'name'     				=> 'Hensel and Gretel Breadcrumbs', // The plugin name
-			'slug'     				=> 'hensel-gretel', // The plugin slug (typically the folder name)
-			'source'   				=> get_stylesheet_directory() . '/library/plugins/hansel-gretel.zip', // The plugin source
-			'required' 				=> false, // If false, the plugin is only 'recommended' instead of required
-			'version' 				=> '', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
-			'force_activation' 		=> false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch
+			'name'     		=> 'Hensel and Gretel Breadcrumbs', // The plugin name
+			'slug'     		=> 'hensel-gretel', // The plugin slug (typically the folder name)
+			'source'   		=> get_stylesheet_directory() . '/library/plugins/hansel-gretel.zip', // The plugin source
+			'required' 		=> false, // If false, the plugin is only 'recommended' instead of required
+			'version' 		=> '', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
+			'force_activation' 	=> false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch
 			'force_deactivation' 	=> false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins
-			'external_url' 			=> '', // If set, overrides default API URL and points to an external URL
+			'external_url' 		=> '', // If set, overrides default API URL and points to an external URL
 		),
 
 		// This is an example of how to include a plugin from the WordPress Plugin Repository
