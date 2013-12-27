e88ee46a1624acca86e24d2649994c95f659afee
Alex Vasquez <alexjvas@gmail.com>
2013-12-26 20:25:12 -0800

    Make theme more 'child theme aware'
    Using 'function exists' for better child theme compatibility for those using this as a parent theme

--- functions.php
+++ functions.php
@@ -41,11 +41,22 @@ function digistarter_setup() {
 	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'digistarter' ),
-		'menu_class'      => 'sf-menu',
-		'items_wrap'      => '<ul id="%1$s" class="sf-menu">%3$s</ul>'
-	) );
+	if ( !function_exists('dg_register_nav_menus') ) :
+		function dg_register_nav_menus() {
+			register_nav_menus(
+				array(
+					'primary-navigation' => __( 'Primary Menu', 'digistarter' ),
+				)
+			);
+		}
+		add_action( 'init', 'dg_register_nav_menus' );
+	endif;
+
+	// register_nav_menus( array(
+	// 	'primary' => __( 'Primary Menu', 'digistarter' ),
+	// 	'menu_class'      => 'sf-menu',
+	// 	'items_wrap'      => '<ul id="%1$s" class="sf-menu">%3$s</ul>'
+	// ) );
 
 	class My_Walker extends Walker_Nav_Menu {
 
@@ -91,79 +102,87 @@ add_action( 'after_setup_theme', 'digistarter_setup' );
 /**
  * Register widgetized area and update sidebar with default widgets.
  */
-function digistarter_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'digistarter' ),
-		'id'            => 'sidebar-1',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h4 class="widget-title">',
-		'after_title'   => '</h4>',
-	) );
-}
-add_action( 'widgets_init', 'digistarter_widgets_init' );
+if ( !function_exists('digistarter_widgets_init') ) :
+	function digistarter_widgets_init() {
+		register_sidebar( array(
+			'name'          => __( 'Sidebar', 'digistarter' ),
+			'id'            => 'sidebar-1',
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
+			'before_title'  => '<h4 class="widget-title">',
+			'after_title'   => '</h4>',
+		) );
+	}
+	add_action( 'widgets_init', 'digistarter_widgets_init' );
+endif;
 
 /**
  * Enqueue scripts and styles.
  */
-function digistarter_scripts() {
-	if (!is_admin()) {
-		wp_enqueue_script('jquery');
+if ( !function_exists('digistarter_scripts') ) :
+	function digistarter_scripts() {
+		if (!is_admin()) {
+			wp_enqueue_script('jquery');
+		}
+
+		// Main Style
+		wp_enqueue_style( 'digistarter-style', get_stylesheet_uri() );
+
+		// Superfish Style
+		wp_enqueue_style( 'superfish', get_template_directory_uri() . '/library/css/superfish/superfish.css' );
+
+		// Meanmenu Style
+		wp_enqueue_style( 'meanmenu', get_template_directory_uri() . '/library/css/meanmenu/meanmenu.css' );
+
+		// Selectivizr Scripts
+		wp_register_script( 'selectivizr', get_stylesheet_directory_uri() . '/library/js/selectivizr/selectivizr-min.js', array(), '1.0.0', false );
+		wp_enqueue_script( 'selectivizr' );
+
+		// Superfish Scripts
+		wp_register_script( 'hoverintent', get_template_directory_uri() . '/library/js/superfish/hoverintent.js', array(), '1.0.0', false );
+		wp_enqueue_script( 'hoverintent' );
+
+		wp_register_script( 'supersubs', get_template_directory_uri() . '/library/js/superfish/supersubs.js', array(), '1.0.0', false );
+		wp_enqueue_script( 'supersubs' );
+
+		wp_register_script( 'superfish', get_template_directory_uri() . '/library/js/superfish/superfish.js', array(), '1.0.0', false );
+		wp_enqueue_script( 'superfish' );
+
+		// Meanmenu Script
+		wp_register_script( 'meanmenu', get_template_directory_uri() . '/library/js/meanmenu/jquery.meanmenu.js', array(), '1.0.0', false );
+		wp_enqueue_script( 'meanmenu' );
+
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+			wp_enqueue_script( 'comment-reply' );
+		}
 	}
+	add_action( 'wp_enqueue_scripts', 'digistarter_scripts' );
+endif;
 
-	// Main Style
-	wp_enqueue_style( 'digistarter-style', get_stylesheet_uri() );
+if ( !function_exists('dg_add_superfish') ) :
+	function dg_add_superfish(){ ?>
+		<script>
+			// Init Superfish
+			jQuery(document).ready(function() {
+				jQuery('ul.sf-menu').superfish();
+			});
+		</script>
+	<?php }
+	add_action( 'wp_head', 'dg_add_superfish' );
+endif;
 
-	// Superfish Style
-	wp_enqueue_style( 'superfish', get_template_directory_uri() . '/library/css/superfish/superfish.css' );
-
-	// Meanmenu Style
-	wp_enqueue_style( 'meanmenu', get_template_directory_uri() . '/library/css/meanmenu/meanmenu.css' );
-
-	// Selectivizr Scripts
-	wp_register_script( 'selectivizr', get_stylesheet_directory_uri() . '/library/js/selectivizr/selectivizr-min.js', array(), '1.0.0', false );
-	wp_enqueue_script( 'selectivizr' );
-
-	// Superfish Scripts
-	wp_register_script( 'hoverintent', get_template_directory_uri() . '/library/js/superfish/hoverintent.js', array(), '1.0.0', false );
-	wp_enqueue_script( 'hoverintent' );
-
-	wp_register_script( 'supersubs', get_template_directory_uri() . '/library/js/superfish/supersubs.js', array(), '1.0.0', false );
-	wp_enqueue_script( 'supersubs' );
-
-	wp_register_script( 'superfish', get_template_directory_uri() . '/library/js/superfish/superfish.js', array(), '1.0.0', false );
-	wp_enqueue_script( 'superfish' );
-
-	// Meanmenu Script
-	wp_register_script( 'meanmenu', get_template_directory_uri() . '/library/js/meanmenu/jquery.meanmenu.js', array(), '1.0.0', false );
-	wp_enqueue_script( 'meanmenu' );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'digistarter_scripts' );
-
-function dg_add_superfish(){ ?>
-	<script>
-		// Init Superfish
-		jQuery(document).ready(function() {
-			jQuery('ul.sf-menu').superfish();
-		});
-	</script>
-<?php }
-add_action( 'wp_head', 'dg_add_superfish' );
-
-function dg_add_meanmenu() { ?>
-	<script>
-		// Init Mean Menu
-		jQuery(document).ready(function () {
-		    jQuery('header nav').meanmenu(
-		    );
-		});
-	</script>
-<?php }
-add_action( 'wp_head', 'dg_add_meanmenu' );
+if ( !function_exists('dg_add_meanmenu') ) :
+	function dg_add_meanmenu() { ?>
+		<script>
+			// Init Mean Menu
+			jQuery(document).ready(function () {
+			    jQuery('header nav').meanmenu(
+			    );
+			});
+		</script>
+	<?php }
+	add_action( 'wp_head', 'dg_add_meanmenu' );
+endif;
 
 /**
  * Including Theme Hook Alliance (https://github.com/zamoose/themehookalliance).
