7b74e25b1628275b66f099106b0faaececc9c267
Alex Vasquez <alexjvas@gmail.com>
2013-12-29 14:08:16 -0800

    adding a few default meanmenu options

--- functions.php
+++ functions.php
@@ -168,8 +168,12 @@ if ( !function_exists('dg_add_meanmenu') ) :
 		<script>
 			// Init Mean Menu
 			jQuery(document).ready(function () {
-			    jQuery('header nav').meanmenu(
-			    );
+			    jQuery('header nav').meanmenu( {
+			    	 meanScreenWidth: "480",
+	   			 meanRevealPosition: "right",
+	   			 meanExpand: ""
+
+			    } );
 			});
 		</script>
 	<?php }
@@ -317,3 +321,29 @@ add_action( 'tha_content_top', 'neat_add_breadcrumbs' ); {
 	}
 }
 
+/**
+ * WP Customizer Stuff d
+ */
+
+function tcx_register_theme_customizer( $wp_customize ) {
+	// More to come...
+	$wp_customize->add_setting(
+		'tcx_link_color',
+			array(
+				'default'     => '#000000'
+		)
+	);
+
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'link_color',
+			array(
+				'label'      => __( 'Link Color', 'tcx' ),
+				'section'    => 'colors',
+				'settings'   => 'tcx_link_color'
+			)
+		)
+	);
+}
+add_action( 'customize_register', 'tcx_register_theme_customizer' );
