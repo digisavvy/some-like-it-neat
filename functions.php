895490236df440bb9c0b56eedb6547b03e22256d
alex vasquez <alex@digisavvy.com>
2013-12-31 01:03:22 -0800

    moving customizer stuff into new branch

--- functions.php
+++ functions.php
@@ -322,30 +322,3 @@ add_action( 'tha_content_top', 'neat_add_breadcrumbs' ); {
 		}
 	}
 }
-
-/**
- * WP Customizer Stuff d
- */
-
-function tcx_register_theme_customizer( $wp_customize ) {
-	// More to come...
-	$wp_customize->add_setting(
-		'tcx_link_color',
-			array(
-				'default'     => '#000000'
-		)
-	);
-
-	$wp_customize->add_control(
-		new WP_Customize_Color_Control(
-			$wp_customize,
-			'link_color',
-			array(
-				'label'      => __( 'Link Color', 'tcx' ),
-				'section'    => 'colors',
-				'settings'   => 'tcx_link_color',
-			)
-		)
-	);
-}
-add_action( 'customize_register', 'tcx_register_theme_customizer' );
