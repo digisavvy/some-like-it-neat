4a4015202f88f41121dd1230b0e9730229231039
alex vasquez <alex@digisavvy.com>
2014-02-19 17:19:10 -0800

    moving sidebar back to where itw as previously

--- page-templates/template-left-col.php
+++ page-templates/template-left-col.php
@@ -13,8 +13,6 @@
 
 get_header(); ?>
 
-	<?php get_sidebar(); ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -43,5 +41,5 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
