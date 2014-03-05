831ee548d1e09e5f94db2fe00886267c8429be31
alex vasquez <alex@digisavvy.com>
2014-03-04 16:35:42 -0800

    adding text domain for translation purposes

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function digistarter_setup() {
 	if ( !function_exists('dg_register_nav_menus') ) :
 		function dg_register_nav_menus() {
 
-			register_nav_menu( 'primary-navigation', __( 'Primary Menu' ) );
+			register_nav_menu( 'primary-navigation', __( 'Primary Menu', 'digistarter' ) );
 
 		}
 		add_action( 'init', 'dg_register_nav_menus' );
@@ -266,7 +266,6 @@ function neat_recommended_required_plugins() {
 	 * end of each line for what each argument will be.
 	 */
 	$config = array(
-		'domain'       		=> $theme_text_domain,         	// Text domain - likely want to be the same as your theme.
 		'default_path' 		=> '',                         	// Default absolute path to pre-packaged plugins
 		'parent_menu_slug' 	=> 'themes.php', 				// Default parent menu slug
 		'parent_url_slug' 	=> 'themes.php', 				// Default parent URL slug
@@ -275,10 +274,10 @@ function neat_recommended_required_plugins() {
 		'is_automatic'    	=> true,					   	// Automatically activate plugins after installation or not
 		'message' 			=> '',							// Message to output right before the plugins table
 		'strings'      		=> array(
-			'page_title'                       			=> __( 'Install Required Plugins', $theme_text_domain ),
-			'menu_title'                       			=> __( 'Install Plugins', $theme_text_domain ),
-			'installing'                       			=> __( 'Installing Plugin: %s', $theme_text_domain ), // %1$s = plugin name
-			'oops'                             			=> __( 'Something went wrong with the plugin API.', $theme_text_domain ),
+			'page_title'                       			=> __( 'Install Required Plugins', 'digistarter' ),
+			'menu_title'                       			=> __( 'Install Plugins', 'digistarter' ),
+			'installing'                       			=> __( 'Installing Plugin: %s', 'digistarter' ), // %1$s = plugin name
+			'oops'                             			=> __( 'Something went wrong with the plugin API.', 'digistarter' ),
 			'notice_can_install_required'     		=> _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s)
 			'notice_can_install_recommended'		=> _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s)
 			'notice_cannot_install'  			=> _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s)
@@ -289,9 +288,9 @@ function neat_recommended_required_plugins() {
 			'notice_cannot_update' 			=> _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s)
 			'install_link' 					=> _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
 			'activate_link' 				  	=> _n_noop( 'Activate installed plugin', 'Activate installed plugins' ),
-			'return'                           			=> __( 'Return to Required Plugins Installer', $theme_text_domain ),
-			'plugin_activated'                 			=> __( 'Plugin activated successfully.', $theme_text_domain ),
-			'complete' 					=> __( 'All plugins installed and activated successfully. %s', $theme_text_domain ), // %1$s = dashboard link
+			'return'                           			=> __( 'Return to Required Plugins Installer', 'digistarter' ),
+			'plugin_activated'                 			=> __( 'Plugin activated successfully.', 'digistarter' ),
+			'complete' 					=> __( 'All plugins installed and activated successfully. %s', 'digistarter' ), // %1$s = dashboard link
 			'nag_type'					=> 'updated' // Determines admin notice type - can only be 'updated' or 'error'
 		)
 	);
