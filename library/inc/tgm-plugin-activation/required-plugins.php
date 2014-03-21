a28ab714de5520a3f09d9c8a2b0b5d46faed19da
Alex Vasquez <alexjvas@gmail.com>
2014-03-21 13:25:35 -0700

    moving tgm plugin code off to its own file and calling it

--- library/inc/tgm-plugin-activation/required-plugins.php
+++ library/inc/tgm-plugin-activation/required-plugins.php
@@ -42,31 +42,36 @@ function my_theme_register_required_plugins() {
      */
     $plugins = array(
 
-        // This is an example of how to include a plugin pre-packaged with a theme.
-        array(
-            'name'               => 'TGM Example Plugin', // The plugin name.
-            'slug'               => 'tgm-example-plugin', // The plugin slug (typically the folder name).
-            'source'             => get_stylesheet_directory() . '/lib/plugins/tgm-example-plugin.zip', // The plugin source.
-            'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-            'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
-            'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-            'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-            'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-        ),
+        // // This is an example of how to include a plugin pre-packaged with a theme.
+        // array(
+        //     'name'               => 'Functionality Plugin', // The plugin name.
+        //     'slug'               => 'functionality-plugin', // The plugin slug (typically the folder name).
+        //     'source'             => get_stylesheet_directory() . '/library/inc/tgm-plugin-activation/plugins/functionality-plugin.zip', // The plugin source.
+        //     'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+        //     'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
+        //     'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+        //     'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+        //     'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+        // ),
 
-        // This is an example of how to include a plugin from a private repo in your theme.
-        array(
-            'name'               => 'TGM New Media Plugin', // The plugin name.
-            'slug'               => 'tgm-new-media-plugin', // The plugin slug (typically the folder name).
-            'source'             => 'https://s3.amazonaws.com/tgm/tgm-new-media-plugin.zip', // The plugin source.
-            'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-            'external_url'       => 'https://github.com/thomasgriffin/New-Media-Image-Uploader', // If set, overrides default API URL and points to an external URL.
-        ),
+        // // This is an example of how to include a plugin from a private repo in your theme.
+        // array(
+        //     'name'               => 'TGM New Media Plugin', // The plugin name.
+        //     'slug'               => 'tgm-new-media-plugin', // The plugin slug (typically the folder name).
+        //     'source'             => 'https://s3.amazonaws.com/tgm/tgm-new-media-plugin.zip', // The plugin source.
+        //     'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+        //     'external_url'       => 'https://github.com/thomasgriffin/New-Media-Image-Uploader', // If set, overrides default API URL and points to an external URL.
+        // ),
 
         // This is an example of how to include a plugin from the WordPress Plugin Repository.
         array(
-            'name'      => 'BuddyPress',
-            'slug'      => 'buddypress',
+            'name'      => 'Hensel & Gretel Breadcrumbs',
+            'slug'      => 'hansel-gretel',
+            'required'  => false,
+        ),
+        array(
+            'name'      => 'Webmaster User Role',
+            'slug'      => 'webmaster-user-role',
             'required'  => false,
         ),
 
