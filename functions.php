56828dee3a74387e077c1f3782b0ec8c0128e0c4
alex vasquez <alex@digisavvy.com>
2014-04-08 21:57:37 -0700

    added parameters for flexnav

--- functions.php
+++ functions.php
@@ -121,6 +121,12 @@ if ( !function_exists('dg_add_flexnav') ) :
 			// Init Flexnav Menu
 			jQuery(document).ready(function($){
 				   $(".flexnav").flexNav({
+				   	'animationSpeed' : 250, // default drop animation speed
+					'transitionOpacity': true, // default opacity animation
+					'buttonSelector': '.menu-button', // default menu button class
+					'hoverIntent': true, // use with hoverIntent plugin
+					'hoverIntentTimeout': 950, // hoverIntent default timeout
+					'calcItemWidths': false // dynamically calcs top level nav item widths
 				});
 			});
 		</script>
