945ae065a7b7d07506bfa8ad7adbd0e9271668cf
alex vasquez <alex@digisavvy.com>
2014-06-04 22:20:46 -0700

    correcting kirki url path

--- library/inc/wp-customizer/customizer.php
+++ library/inc/wp-customizer/customizer.php
@@ -210,7 +210,7 @@ function shoestrap_customizer_config() {
         // If Kirki is embedded in your theme, then you can use this line to specify its location.
         // This will be used to properly enqueue the necessary stylesheets and scripts.
         // If you are using kirki as a plugin then please delete this line.
-        'url_path'     => get_template_directory_uri() . '/library/inc/kirki',
+        'url_path'     => get_template_directory_uri() . '/library/inc/kirki/',
 
         // If you want to take advantage of the backround control's 'output',
         // then you'll have to specify the ID of your stylesheet here.
@@ -224,6 +224,46 @@ function shoestrap_customizer_config() {
 
 }
 add_filter( 'kirki/config', 'shoestrap_customizer_config' );
+
+/**
+ * Kirki Customizer Settings
+ */
+
+function my_custom_section( $wp_customize ) {
+
+    // Create the "My Section" section
+    $wp_customize->add_section( 'my_section', array(
+        'title'    => __( 'My Section', 'translation_domain' ),
+        'priority' => 12
+    ) );
+
+}
+add_action( 'customize_register', 'my_custom_section' );
+
+/**
+ * Create the setting
+ */
+function my_custom_setting( $controls ) {
+
+    $controls[] = array(
+    'type'     => 'radio',
+    'mode'     => 'buttonset',
+    'setting'  => 'my_setting',
+    'label'    => __( 'My Setting', 'textdomain' ),
+    'section'  => 'my_section',
+    'default'  => 'option_1',
+    'priority' => 1,
+    'choices'  => array(
+        'option_1' => __( 'Option 1', 'textdomain' ),
+        'option_2' => __( 'Option 2', 'textdomain' ),
+        'option_3' => __( 'Option 3', 'textdomain' ),
+        'option_4' => __( 'Option 4', 'textdomain' ),
+    ),
+);
+
+    return $controls;
+}
+add_filter( 'kirki/controls', 'my_custom_setting' );
 
 
 /**
