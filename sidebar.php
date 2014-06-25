3c20dd8ef1b2aeb4017e951da8b099c0a294c74b
Robert Neu <rob@auditwp.com>
2014-06-24 23:22:49 -0500

    Reorganized theme files, still broken

--- sidebar.php
+++ sidebar.php
@@ -5,33 +5,33 @@
  * @package digistarter
  */
 ?>
-	<?php tha_sidebars_before(); ?>
-	<div id="secondary" class="widget-area" role="complementary">
-		<?php tha_sidebar_top(); ?>
-		<?php do_action( 'before_sidebar' ); ?>
-		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+<?php tha_sidebars_before(); ?>
+<div id="secondary" class="widget-area" role="complementary">
+	<?php tha_sidebar_top(); ?>
+	<?php do_action( 'before_sidebar' ); ?>
+	<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-			<aside id="search" class="widget widget_search">
-				<?php get_search_form(); ?>
-			</aside>
+		<aside id="search" class="widget widget_search">
+			<?php get_search_form(); ?>
+		</aside>
 
-			<aside id="archives" class="widget">
-				<h4 class="widget-title"><?php _e( 'Archives', 'digistarter' ); ?></h4>
-				<ul>
-					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-				</ul>
-			</aside>
+		<aside id="archives" class="widget">
+			<h4 class="widget-title"><?php _e( 'Archives', 'digistarter' ); ?></h4>
+			<ul>
+				<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+			</ul>
+		</aside>
 
-			<aside id="meta" class="widget">
-				<h4 class="widget-title"><?php _e( 'Meta', 'digistarter' ); ?></h4>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</aside>
+		<aside id="meta" class="widget">
+			<h4 class="widget-title"><?php _e( 'Meta', 'digistarter' ); ?></h4>
+			<ul>
+				<?php wp_register(); ?>
+				<li><?php wp_loginout(); ?></li>
+				<?php wp_meta(); ?>
+			</ul>
+		</aside>
 
-		<?php endif; // end sidebar widget area ?>
-		<?php tha_sidebar_bottom(); ?>
-	</div><!-- #secondary -->
-	<?php tha_sidebars_after(); ?>
+	<?php endif; // end sidebar widget area ?>
+	<?php tha_sidebar_bottom(); ?>
+</div><!-- #secondary -->
+<?php tha_sidebars_after(); ?>
