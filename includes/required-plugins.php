6625bc06404b6b204e28457f65c66afaae1cc482
Robert Neu <rob@auditwp.com>
2014-06-25 04:18:11 -0500

    Moved vendor files

--- includes/required-plugins.php
+++ includes/required-plugins.php
@@ -19,7 +19,6 @@
 /**
  * Include the TGM_Plugin_Activation class.
  */
-require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
 
 add_action( 'tgmpa_register', 'my_theme_register_required_plugins' );
 /**
