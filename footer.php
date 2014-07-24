e7cc3ebd3cec285475fb32706be5dae78ac92e58
alex vasquez <alex@digisavvy.com>
2014-07-23 23:50:21 -0700

    placing wp_footer appropriately

--- footer.php
+++ footer.php
@@ -13,7 +13,6 @@
    		<?php tha_footer_before(); ?>
 		<footer id="colophon" class="site-footer" role="contentinfo" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
 			<?php tha_footer_top(); ?>
-			<?php wp_footer(); ?>
 			<div class="site-info">
 				<?php do_action( 'digistarter_credits' ); ?>
 				<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by <span class="genericon genericon-wordpress"></span> %s', 'digistarter' ), 'WordPress' ); ?></a>
@@ -23,11 +22,11 @@
 			</div><!-- .site-info -->
 			<?php tha_footer_bottom(); ?>
 		</footer><!-- #colophon -->
-
+		<?php tha_footer_after(); ?>
 	</div><!-- .wrap -->
 </div><!-- #page -->
 
 <?php tha_body_bottom(); ?>
-<?php tha_footer_after(); ?>
+<?php wp_footer(); ?>
 </body>
 </html>
