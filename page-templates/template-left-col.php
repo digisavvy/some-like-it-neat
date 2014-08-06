70585f33fcba080a791659559668fb5bfbdca5f4
alex vasquez <alexjvas@gmail.com>
2014-08-05 20:06:53 -0700

    Fix get_template_part() path

--- page-templates/template-left-col.php
+++ page-templates/template-left-col.php
@@ -26,7 +26,7 @@ get_header(); ?>
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
-					get_template_part( 'partials/content', 'page' );
+					get_template_part( 'page-templates/partials/content', 'page' );
 				?>
 
 			<?php endwhile; ?>
