28fcdd41d1092a1b40a6361750d7016ecf4f2162
alex vasquez <alex@digisavvy.com>
2014-09-21 23:53:27 -0700

    Added run sequence and combine media query plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
-// Project configuration
-var project   = 'my-theme';
-
 // Load plugins
 var gulp = require('gulp'),
     browserSync = require('browser-sync'),
-    reload      = browserSync.reload, 
-    livereload = require('gulp-livereload'),
+    reload      = browserSync.reload,
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
@@ -16,8 +12,21 @@ var gulp = require('gulp'),
     clean = require('gulp-clean'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
+    cmq = require('gulp-combine-media-queries'),
+    runSequence = require('gulp-run-sequence'),
     cache = require('gulp-cache');
-    // build     = './'+project+'/';
+
+//Error Handler
+var handleErrors = function() {
+    // Send error to notification center with gulp-notify
+    notify.onError({
+    title: "Compile Error",
+    message: "<%= error.message %>"
+    }).apply(this, arguments);
+
+    // Keep gulp from hanging on this task
+    this.emit('end');
+};
 
 // Browser Sync
 gulp.task('browser-sync', function() {
@@ -32,14 +41,18 @@ gulp.task('browser-sync', function() {
 // Styles
 gulp.task('styles', function() {
   return gulp.src('library/assets/sass/style.scss')
+    //send SASS errors to console
+    .on('error', handleErrors)
     .pipe(sass({ style: 'expanded', }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('./'))
     .pipe(reload({stream:true}))
-    .pipe(rename({ suffix: '.min' }))
+    .pipe(rename({ suffix: '-min' }))
     .pipe(minifycss({keepBreaks:true}))
+    .pipe(minifycss({ keepSpecialComments: 1 }))
     .pipe(gulp.dest('dist/styles'))
-    // .pipe(gulp.dest(build))
+    //combine media queries
+    .pipe(cmq())
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
@@ -50,7 +63,7 @@ gulp.task('scripts', function() {
     // .pipe(jshint.reporter('default'))
     .pipe(concat('production.js'))
     .pipe(gulp.dest('dist/scripts'))
-    .pipe(rename({ suffix: '.min' }))
+    .pipe(rename({ suffix: '-min' }))
     .pipe(uglify())
     // .pipe(gulp.dest(build))
     .pipe(gulp.dest('dist/scripts'))
