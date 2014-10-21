24c21f2f682c8c37cfaf5204383ce2241bc3565a
alex vasquez <alex@digisavvy.com>
2014-10-20 22:37:50 -0700

    standardizing script prefixing

--- header.php
+++ header.php
@@ -34,11 +34,11 @@
 			</div>
 
 			<nav id="primary-nav" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
-			<div class="menu-button"><span><?php echo get_theme_mod( 'neat_mobile_nav_label' ); ?></span></div>
+			<div class="menu-button"><span><?php echo get_theme_mod( 'digistarter_mobile_nav_label' ); ?></span></div>
 			<?php 	wp_nav_menu( array(
 				    'theme_location' => 'primary-navigation',
 				    'menu_class' => 'flexnav', //Adding the class for FlexNav
-				    'items_wrap' => '<ul data-breakpoint=" '. esc_attr( get_theme_mod( 'neat_mobile_min_width' ) ) .' " id="%1$s" class="%2$s">%3$s</ul>', // Adding data-breakpoint for FlexNav
+				    'items_wrap' => '<ul data-breakpoint=" '. esc_attr( get_theme_mod( 'digistarter_mobile_min_width' ) ) .' " id="%1$s" class="%2$s">%3$s</ul>', // Adding data-breakpoint for FlexNav
 				    ));
 			?>
 
