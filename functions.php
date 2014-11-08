d7d58a96f8bd2191314b8cbe537c419904a423bf
alex vasquez <alexjvas@gmail.com>
2014-11-08 10:46:17 -0800

    equeing builtin dashicons instead

--- functions.php
+++ functions.php
@@ -102,7 +102,7 @@ if ( !function_exists('digistarter_scripts') ) :
 		wp_enqueue_style( 'digistarter-style',  get_stylesheet_directory_uri() . '/assets/css/style.css' );
 
 		// Dashicons
-		 wp_enqueue_style( 'dashicons', get_stylesheet_directory_uri() . '/assets/css/dashicons.css' );
+		 wp_enqueue_style( 'dashicons' );
 
 		// Concatonated Scripts
 		wp_enqueue_script( 'production-js', get_template_directory_uri() . '/assets/js/production-min.js', array( 'jquery' ), '1.0.0', false );
