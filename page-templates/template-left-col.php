590f5769c92d3b56c89bc6e2ee3029d5a146bc9b
alex vasquez <alex@digisavvy.com>
2014-11-26 22:48:54 -0800

    adding comments template

--- page-templates/template-left-col.php
+++ page-templates/template-left-col.php
@@ -29,6 +29,13 @@ get_header(); ?>
 					get_template_part( 'page-templates/partials/content', 'page' );
 				?>
 
+				<?php
+				        // If comments are open or we have at least one comment, load up the comment template
+				        if ( comments_open() || '0' != get_comments_number() ) :
+				                comments_template();
+				        endif;
+				?>
+
 			<?php endwhile; ?>
 
 			<?php digistarter_paging_nav(); ?>
