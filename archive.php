c973cadce21744a978cd23a8adc0f9afa908e55b
alex vasquez <alexjvas@gmail.com>
2014-12-22 10:35:08 -0800

    spacing

--- archive.php
+++ archive.php
@@ -84,7 +84,7 @@ get_header(); ?>
 					// Show an optional term description.
 					$term_description = term_description();
 					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
+						printf( '<div class="taxonomy-description">%s</div>', 	$term_description );
 					endif;
 				?>
 			</header><!-- .page-header -->
