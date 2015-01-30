e10e95764b927fb3f7038623953c7a9585d34f2d
Udit Desai <desaiuditd@gmail.com>
2015-01-31 02:33:55 +0530

    Revert "Fixed PHPCS Issues"
    This reverts commit ebb3e721c9fbc1aab9cc8013d6cf3c7029f600a5.

--- single.php
+++ single.php
@@ -18,8 +18,8 @@ get_header(); ?>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
-			if ( comments_open() || '0' != get_comments_number() ) :
-				comments_template();
+				if ( comments_open() || '0' != get_comments_number() ) :
+					comments_template();
 				endif;
 			?>
 
