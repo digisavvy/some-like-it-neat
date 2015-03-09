41824257faf5c95acf2728864ae43f692b348e65
alex vasquez <alex@digisavvy.com>
2015-03-08 20:56:19 -0700

    move main selector to wrap around primary and secondary content

--- page.php
+++ page.php
@@ -13,7 +13,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<div id="content" class="site-content">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
@@ -29,7 +29,7 @@ get_header(); ?>
 			<?php
 endwhile; // end of the loop. ?>
 
-		</main><!-- #main -->
+		</div><!-- #content -->
 	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
