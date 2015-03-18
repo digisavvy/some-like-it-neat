570bed12908c353585d166f8b0292cb6c2ddf7f5
alex vasquez <alexjvas@gmail.com>
2015-03-17 17:37:46 -0700

    fix project URL issue
    The variable should hold the whole project url string; nothing should be hardcoded beyond the config area.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@
 */
 
 // Project configuration
-var project 	= 'somelikeitneat', // Optional - Use your own project name here...
+var project 	= 'somelikeitneat.dev', // Should be the URL of YOUR local project without a prefix i.e. example.local
 	build 		= './build/', // Files that you want to package into a zip go here
 	source 		= './assets/', 	// Your main project assets and naming 'source' instead of 'src' to avoid confusion with gulp.src
 	bower 		= './assets/bower_components/', // Not truly using this yet, more or less playing right now. TO-DO Place in Dev branch
@@ -49,7 +49,7 @@ gulp.task('browser-sync', function() {
 		'**/*.php'
 	];
 	browserSync.init(files, {
-		proxy: project+".dev"
+		proxy: project+
 	});
 });
 
