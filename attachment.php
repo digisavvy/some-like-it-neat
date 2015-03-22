25bc63878476a8655522f72e4c6872ad05408181
alex vasquez <alex@digisavvy.com>
2015-03-22 15:57:04 -0700

    adding attachment template

---
+++ attachment.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying all attachments.
+ *
+ * This is the template that displays all attachments by default.
+ * Please note that this is the WordPress construct of attachments
+ * and that other 'attachments' on your WordPress site will use a
+ * different template.
+ *
+ * @package some_like_it_neat
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'page-templates/partials/content', 'attachment' ); ?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ page-templates/partials/content-attachment.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template used for displaying attachment (image) content in attachment.php
+ *
+ * @package some_like_it_neat
+ */
+?>
+<?php tha_entry_before(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope itemType="http://schema.org/WebPage">
+	<?php tha_entry_top(); ?>
+	<header class="entry-header">
+
+		<h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1>
+
+	</header><!-- .entry-header -->
+
+
+	<div class="entry-content" itemprop="mainContentOfPage">
+
+	<?php the_content(); ?>
+
+	<div class="entry-caption">
+		<?php echo wp_get_attachment_image( get_the_ID(), 'large' ); ?>
+		<?php if ( ! empty( $post->post_excerpt ) ) the_excerpt(); ?>
+		<p class='resolutions'> Downloads:
+		<?php
+			$images = array();
+			$image_sizes = get_intermediate_image_sizes();
+			array_unshift( $image_sizes, 'full' );
+			foreach( $image_sizes as $image_size ) {
+				$image = wp_get_attachment_image_src( get_the_ID(), $image_size );
+				$name = $image_size . ' (' . $image[1] . 'x' . $image[2] . ')';
+				$images[] = '<a href="' . $image[0] . '">' . $name . '</a>';
+			}
+
+			echo implode( ' | ', $images );
+		?>
+		</p>
+	</div>
+
+
+
+	</div><!-- .entry-content -->
+	<?php edit_post_link( __( 'Edit', 'some-like-it-neat' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+	<?php tha_entry_bottom(); ?>
+</article><!-- #post-## -->
+<?php tha_entry_after(); ?>
