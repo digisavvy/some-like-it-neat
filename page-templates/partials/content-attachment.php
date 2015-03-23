14db262b0d4380027503f57c9e7392359c3e7191
alex vasquez <alex@digisavvy.com>
2015-03-22 18:45:49 -0700

    formatting file

--- page-templates/partials/content-attachment.php
+++ page-templates/partials/content-attachment.php
@@ -20,28 +20,31 @@
 	<?php the_content(); ?>
 
 	<div class="entry-caption">
-		<?php echo wp_get_attachment_image( get_the_ID(), 'large' ); ?>
-		<?php if ( ! empty( $post->post_excerpt ) ) the_excerpt(); ?>
+	<?php echo wp_get_attachment_image( get_the_ID(), 'large' ); ?>
+	<?php if ( ! empty( $post->post_excerpt ) ) {
+		the_excerpt();
+}
+	?>
 		<p class='resolutions'> Downloads:
-		<?php
-			$images = array();
-			$image_sizes = get_intermediate_image_sizes();
-			array_unshift( $image_sizes, 'full' );
-			foreach( $image_sizes as $image_size ) {
-				$image = wp_get_attachment_image_src( get_the_ID(), $image_size );
-				$name = $image_size . ' (' . $image[1] . 'x' . $image[2] . ')';
-				$images[] = '<a href="' . $image[0] . '">' . $name . '</a>';
-			}
+	<?php
+	$images = array();
+	$image_sizes = get_intermediate_image_sizes();
+	array_unshift( $image_sizes, 'full' );
+	foreach ( $image_sizes as $image_size ) {
+		$image = wp_get_attachment_image_src( get_the_ID(), $image_size );
+		$name = $image_size . ' (' . $image[1] . 'x' . $image[2] . ')';
+		$images[] = '<a href="' . $image[0] . '">' . $name . '</a>';
+	}
 
-			echo implode( ' | ', $images );
-		?>
+	echo implode( ' | ', $images );
+	?>
 		</p>
 	</div>
 
 
 
 	</div><!-- .entry-content -->
-	<?php edit_post_link( __( 'Edit', 'some-like-it-neat' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
-	<?php tha_entry_bottom(); ?>
+    <?php edit_post_link( __( 'Edit', 'some-like-it-neat' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+    <?php tha_entry_bottom(); ?>
 </article><!-- #post-## -->
 <?php tha_entry_after(); ?>
