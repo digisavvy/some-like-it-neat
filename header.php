821d100661630d7c3600a7ad8d9d584f00af37be
alex vasquez <alex@digisavvy.com>
2015-04-28 08:16:46 -0700

    fixing odd title-tag issue by hardcoding it for now

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<style type="text/css">
 		<?php if ( 'no' === get_theme_mod( 'some-like-it-neat_post_format_support' ) ): ?>
 			h1.entry-title:before {
