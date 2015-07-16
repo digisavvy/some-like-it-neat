2ce8e487ed1dffa30639bfc5b0766985440bb893
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2015-07-16 08:40:08 -0500

    Remove HTML comments and align markup

--- 404.php
+++ 404.php
@@ -4,8 +4,8 @@
  *
  * @package digistarter
  */
-
-get_header(); ?>
+get_header();
+?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -13,7 +13,7 @@ get_header(); ?>
 			<section class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'digistarter' ); ?></h1>
-				</header><!-- .page-header -->
+				</header>
 
 				<div class="page-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'digistarter' ); ?></p>
@@ -46,11 +46,9 @@ get_header(); ?>
 					?>
 
 					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				</div>
+			</section>
+		</main>
+	</div>
 
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -10,15 +10,14 @@
  *
  * @package digistarter
  */
-
-get_header(); ?>
+get_header();
+?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
-			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
@@ -39,8 +38,8 @@ get_header(); ?>
 
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
+	</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
