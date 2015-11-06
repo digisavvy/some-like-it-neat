4da2af2b574195b1a87642bb585f433e8ac0f4b1
JJ Loomis <jjloomis@gmail.com>
2015-11-06 11:15:46 -0600

    Change get_template_directory to get_stylesheet_directory

--- functions.php
+++ functions.php
@@ -180,7 +180,7 @@ if ( ! function_exists( 'some_like_it_neat_scripts' ) ) :
 			// wp_enqueue_script( 'production-js', get_template_directory_uri() . '/assets/js/production-min.js', array( 'jquery' ), '1.0.0', false );
 			
 			// Main Styles
-			wp_enqueue_style( 'some_like_it_neat-style',  get_template_directory_uri() . '/assets/css/style-min.css' );
+			wp_enqueue_style( 'some_like_it_neat-style',  get_stylesheet_directory_uri() . '/assets/css/style-min.css' );
 
 	 endif;
 
