32435b7c718212831df02ec404fdcbccaf44b2eb
alex vasquez <alexjvas@gmail.com>
2015-11-08 20:30:33 -0800

    updating description

--- singular.php
+++ singular.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * The template for displaying all singular posts and pages.
+ * The template for displaying all singular post types.
  *
- * This is the template that gets used in absence of single.php or page.php
- * Is also "true" for is_singular()
+ * This is the template that gets used in absence of single.php, page.php, or a
+ * singular post type template. Is also "true" for is_singular().
  *
  * @package some_like_it_neat
  */
