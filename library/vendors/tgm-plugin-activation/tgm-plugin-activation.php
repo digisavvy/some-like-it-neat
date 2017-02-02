4144c1575ae46ad5a6643ff65c9021870c2ed95d
alex@digisavvy.com <alex@digisavvy.com>
2017-02-02 08:50:21 -0800

    update tgmp to 2.6.1

--- library/vendors/tgm-plugin-activation/tgm-plugin-activation.php
+++ library/vendors/tgm-plugin-activation/tgm-plugin-activation.php
@@ -6,38 +6,54 @@
  * It is expected that theme authors would copy and paste this code into their
  * functions.php file, and amend to suit.
  *
+ * @see http://tgmpluginactivation.com/configuration/ for detailed documentation.
+ *
  * @package    TGM-Plugin-Activation
  * @subpackage Example
- * @version    2.4.0
- * @author     Thomas Griffin <thomasgriffinmedia.com>
- * @author     Gary Jones <gamajo.com>
- * @copyright  Copyright (c) 2014, Thomas Griffin
+ * @version    2.6.1 for parent theme Some Like It Neat for publication on WordPress.org
+ * @author     Thomas Griffin, Gary Jones, Juliette Reinders Folmer
+ * @copyright  Copyright (c) 2011, Thomas Griffin
  * @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
- * @link       https://github.com/thomasgriffin/TGM-Plugin-Activation
+ * @link       https://github.com/TGMPA/TGM-Plugin-Activation
  */
 
 /**
  * Include the TGM_Plugin_Activation class.
+ *
+ * Depending on your implementation, you may want to change the include call:
+ *
+ * Parent Theme:
+ * require_once get_template_directory() . '/path/to/class-tgm-plugin-activation.php';
+ *
+ * Child Theme:
+ * require_once get_stylesheet_directory() . '/path/to/class-tgm-plugin-activation.php';
+ *
+ * Plugin:
+ * require_once dirname( __FILE__ ) . '/path/to/class-tgm-plugin-activation.php';
  */
-require_once dirname(__FILE__) . '/class-tgm-plugin-activation.php';
+require_once get_template_directory() . '/library/vendors/tgm-plugin-activation/class-tgm-plugin-activation.php';
 
-add_action('tgmpa_register', 'my_theme_register_required_plugins');
+add_action( 'tgmpa_register', 'some_like_it_neat_register_required_plugins' );
+
 /**
  * Register the required plugins for this theme.
  *
- * In this example, we register two plugins - one included with the TGMPA library
- * and one from the .org repo.
+ * In this example, we register five plugins:
+ * - one included with the TGMPA library
+ * - two from an external source, one from an arbitrary source, one from a GitHub repository
+ * - two from the .org repo, where one demonstrates the use of the `is_callable` argument
  *
- * The variable passed to tgmpa_register_plugins() should be an array of plugin
- * arrays.
+ * The variables passed to the `tgmpa()` function should be:
+ * - an array of plugin arrays;
+ * - optionally a configuration array.
+ * If you are not changing anything in the configuration array, you can remove the array and remove the
+ * variable from the function call: `tgmpa( $plugins );`.
+ * In that case, the TGMPA default settings will be used.
  *
- * This function is hooked into tgmpa_init, which is fired within the
- * TGM_Plugin_Activation class constructor.
+ * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
  */
-function my_theme_register_required_plugins() 
-{
-
-    /**
+function some_like_it_neat_register_required_plugins() {
+    /*
      * Array of plugin arrays. Required keys are name and slug.
      * If the source is NOT from the .org repo, then source is also required.
      */
@@ -73,43 +89,102 @@ function my_theme_register_required_plugins()
 
     );
 
-    /**
+    /*
      * Array of configuration settings. Amend each line as needed.
-     * If you want the default strings to be available under your own theme domain,
-     * leave the strings uncommented.
-     * Some of the strings are added into a sprintf, so see the comments at the
-     * end of each line for what each argument will be.
+     *
+     * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+     * strings available, please help us make TGMPA even better by giving us access to these translations or by
+     * sending in a pull-request with .po file(s) with the translations.
+     *
+     * Only uncomment the strings in the config array if you want to customize the strings.
      */
     $config = array(
-        'default_path' => '',                      // Default absolute path to pre-packaged plugins.
+        'id'           => 'some-like-it-neat',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+        'default_path' => '',                      // Default absolute path to bundled plugins.
         'menu'         => 'tgmpa-install-plugins', // Menu slug.
         'has_notices'  => true,                    // Show admin notices or not.
         'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
         'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
         'is_automatic' => false,                   // Automatically activate plugins after installation or not.
         'message'      => '',                      // Message to output right before the plugins table.
+
+        /*
         'strings'      => array(
-            'page_title'                      => __('Install Required Plugins', 'tgmpa'),
-            'menu_title'                      => __('Install Plugins', 'tgmpa'),
-            'installing'                      => __('Installing Plugin: %s', 'tgmpa'), // %s = plugin name.
-            'oops'                            => __('Something went wrong with the plugin API.', 'tgmpa'),
-            'notice_can_install_required'     => _n_noop('This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.'), // %1$s = plugin name(s).
-            'notice_can_install_recommended'  => _n_noop('This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.'), // %1$s = plugin name(s).
-            'notice_cannot_install'           => _n_noop('Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.'), // %1$s = plugin name(s).
-            'notice_can_activate_required'    => _n_noop('The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.'), // %1$s = plugin name(s).
-            'notice_can_activate_recommended' => _n_noop('The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.'), // %1$s = plugin name(s).
-            'notice_cannot_activate'          => _n_noop('Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.'), // %1$s = plugin name(s).
-            'notice_ask_to_update'            => _n_noop('The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.'), // %1$s = plugin name(s).
-            'notice_cannot_update'            => _n_noop('Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.'), // %1$s = plugin name(s).
-            'install_link'                    => _n_noop('Begin installing plugin', 'Begin installing plugins'),
-            'activate_link'                   => _n_noop('Begin activating plugin', 'Begin activating plugins'),
-            'return'                          => __('Return to Required Plugins Installer', 'tgmpa'),
-            'plugin_activated'                => __('Plugin activated successfully.', 'tgmpa'),
-            'complete'                        => __('All plugins installed and activated successfully. %s', 'tgmpa'), // %s = dashboard link.
-            'nag_type'                        => 'updated' // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
-        )
+            'page_title'                      => __( 'Install Required Plugins', 'some-like-it-neat' ),
+            'menu_title'                      => __( 'Install Plugins', 'some-like-it-neat' ),
+            /* translators: %s: plugin name. * /
+            'installing'                      => __( 'Installing Plugin: %s', 'some-like-it-neat' ),
+            /* translators: %s: plugin name. * /
+            'updating'                        => __( 'Updating Plugin: %s', 'some-like-it-neat' ),
+            'oops'                            => __( 'Something went wrong with the plugin API.', 'some-like-it-neat' ),
+            'notice_can_install_required'     => _n_noop(
+                /* translators: 1: plugin name(s). * /
+                'This theme requires the following plugin: %1$s.',
+                'This theme requires the following plugins: %1$s.',
+                'some-like-it-neat'
+            ),
+            'notice_can_install_recommended'  => _n_noop(
+                /* translators: 1: plugin name(s). * /
+                'This theme recommends the following plugin: %1$s.',
+                'This theme recommends the following plugins: %1$s.',
+                'some-like-it-neat'
+            ),
+            'notice_ask_to_update'            => _n_noop(
+                /* translators: 1: plugin name(s). * /
+                'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
+                'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
+                'some-like-it-neat'
+            ),
+            'notice_ask_to_update_maybe'      => _n_noop(
+                /* translators: 1: plugin name(s). * /
+                'There is an update available for: %1$s.',
+                'There are updates available for the following plugins: %1$s.',
+                'some-like-it-neat'
+            ),
+            'notice_can_activate_required'    => _n_noop(
+                /* translators: 1: plugin name(s). * /
+                'The following required plugin is currently inactive: %1$s.',
+                'The following required plugins are currently inactive: %1$s.',
+                'some-like-it-neat'
+            ),
+            'notice_can_activate_recommended' => _n_noop(
+                /* translators: 1: plugin name(s). * /
+                'The following recommended plugin is currently inactive: %1$s.',
+                'The following recommended plugins are currently inactive: %1$s.',
+                'some-like-it-neat'
+            ),
+            'install_link'                    => _n_noop(
+                'Begin installing plugin',
+                'Begin installing plugins',
+                'some-like-it-neat'
+            ),
+            'update_link' 					  => _n_noop(
+                'Begin updating plugin',
+                'Begin updating plugins',
+                'some-like-it-neat'
+            ),
+            'activate_link'                   => _n_noop(
+                'Begin activating plugin',
+                'Begin activating plugins',
+                'some-like-it-neat'
+            ),
+            'return'                          => __( 'Return to Required Plugins Installer', 'some-like-it-neat' ),
+            'plugin_activated'                => __( 'Plugin activated successfully.', 'some-like-it-neat' ),
+            'activated_successfully'          => __( 'The following plugin was activated successfully:', 'some-like-it-neat' ),
+            /* translators: 1: plugin name. * /
+            'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'some-like-it-neat' ),
+            /* translators: 1: plugin name. * /
+            'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'some-like-it-neat' ),
+            /* translators: 1: dashboard link. * /
+            'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'some-like-it-neat' ),
+            'dismiss'                         => __( 'Dismiss this notice', 'some-like-it-neat' ),
+            'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'some-like-it-neat' ),
+            'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'some-like-it-neat' ),
+
+            'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
+        ),
+        */
     );
 
-    tgmpa($plugins, $config);
-
+    tgmpa( $plugins, $config );
 }
