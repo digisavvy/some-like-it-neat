39179470f4700af7a07451dbedbb73cf97032530
alex@digisavvy.com <alex@digisavvy.com>
2017-03-26 21:52:35 -0700

    remove spacing

--- functions.php
+++ functions.php
@@ -208,7 +208,6 @@ if ( ! function_exists( 'some_like_it_neat_scripts' ) ) :
 
             if ( ! $filecount == 0 ) {
                 if ( SCRIPT_DEBUG || WP_DEBUG ) :
-
                     // Concatonated Scripts
                     wp_enqueue_script( 'some_like_it_neat-js', get_theme_file_uri( '/assets/js/development.js' ), array( 'jquery' ), '1.0.0', false );
                 else :
