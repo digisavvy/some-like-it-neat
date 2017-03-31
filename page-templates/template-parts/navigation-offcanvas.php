fdfba9f9b385d15b0e930c9c262ac42e999f5b60
alex@digisavvy.com <alex@digisavvy.com>
2017-03-31 07:26:36 -0700

    add offcanvas markup

--- page-templates/template-parts/navigation-offcanvas.php
+++ page-templates/template-parts/navigation-offcanvas.php
@@ -4,4 +4,18 @@
  */
 ?>
 
-<?php echo 'Offcanvas Menu'; ?>
+<nav id="site-navigation" class="main-navigation" role="navigation" <?php //perennial_schema( 'site-primary-menu' ); ?>>
+    <div class="main-navigation-inside">
+        <?php
+        // Header Menu
+        wp_nav_menu( apply_filters( 'perennial_header_menu_args', array(
+            'container'       => 'div',
+            'container_class' => 'site-header-menu',
+            'theme_location'  => 'header-menu',
+            'menu_class'      => 'header-menu',
+            'menu_id'         => 'menu-1',
+            'depth'           => 3,
+        ) ) );
+        ?>
+    </div><!-- .main-navigation-inside -->
+</nav><!-- .main-navigation -->
