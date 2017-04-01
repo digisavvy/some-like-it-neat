da07534f734fc7c83ec42ee27e20440a5f036762
alex@digisavvy.com <alex@digisavvy.com>
2017-03-31 22:58:13 -0700

    remove duplicated markup

--- page-templates/template-parts/navigation-offcanvas-wrap.php
+++ page-templates/template-parts/navigation-offcanvas-wrap.php
@@ -45,18 +45,6 @@
     <?php // endif; ?>
 </div><!-- .site-header-inside-wrapper -->
 
-<nav id="site-navigation" class="main-navigation" role="navigation" <?php //perennial_schema( 'site-primary-menu' ); ?>>
-    <div class="main-navigation-inside">
-        <?php
-        // Header Menu
-        wp_nav_menu( apply_filters( 'perennial_header_menu_args', array(
-            'container'       => 'div',
-            'container_class' => 'site-header-menu',
-            'theme_location'  => 'header-menu',
-            'menu_class'      => 'header-menu',
-            'menu_id'         => 'menu-1',
-            'depth'           => 3,
-        ) ) );
-        ?>
-    </div><!-- .main-navigation-inside -->
-</nav><!-- .main-navigation -->
+
+        <?php // get_template_part( 'page-templates/template-parts/navigation', 'offcanvas-menu' ); ?>
+
