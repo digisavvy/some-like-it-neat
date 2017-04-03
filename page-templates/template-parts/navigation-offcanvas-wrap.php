cd62e71b325dbdf767eb20f61c1ddfe7191e9cae
alex@digisavvy.com <alex@digisavvy.com>
2017-04-02 22:23:48 -0700

    removing unused functions and markup

--- page-templates/template-parts/navigation-offcanvas-wrap.php
+++ page-templates/template-parts/navigation-offcanvas-wrap.php
@@ -14,12 +14,6 @@
     </div><!-- .hamburger-wrapper -->
 
     <div class="site-branding-wrapper">
-        <?php
-        // Site Custom Logo
-        if ( function_exists( 'the_custom_logo' ) ) {
-            the_custom_logo();
-        }
-        ?>
 
         <div class="site-branding">
             <?php if ( is_front_page() && is_home() ) : ?>
@@ -28,17 +22,8 @@
                 <p class="site-title" <?php // perennial_schema( 'site-title' ); ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
             <?php endif; ?>
 
-            <?php
-            $description = get_bloginfo( 'description', 'display' );
-            if ( $description || is_customize_preview() ) :
-                ?>
-                <p class="site-description" <?php // perennial_schema( 'site-description' ); ?>><?php echo esc_html( $description ); /* WPCS: xss ok. */ ?></p>
-            <?php endif; ?>
         </div>
     </div><!-- .site-branding-wrapper -->
 
 </div><!-- .site-header-inside-wrapper -->
-
-
-        <?php // get_template_part( 'page-templates/template-parts/navigation', 'offcanvas-menu' ); ?>
 
