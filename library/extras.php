9f99554d085dad63a3de1114a18ef90c1b9301dd
Alex Vasquez <alexjvas@gmail.com>
2017-06-18 19:54:03 -0700

    add headroom function

--- library/extras.php
+++ library/extras.php
@@ -28,3 +28,115 @@ if (! function_exists('some_like_it_neat_post_navigation') ) :
 
 endif;
 add_action('tha_entry_after', 'some_like_it_neat_post_navigation');
+
+if ( ! function_exists( 'some_like_it_neat_entry_footer' ) ) :
+	/**
+	 * Prints HTML with meta information for the categories, tags and comments.
+	 */
+	function some_like_it_neat_entry_footer() {
+		// Hide category and tag text for pages.
+		if ( 'post' === get_post_type() ) {
+			/* translators: used between list items, there is a space after the comma */
+			$categories_list = get_the_category_list( esc_html__( ', ', '_s' ) );
+			if ( $categories_list && some_like_it_neat_categorized_blog() ) {
+				/* translators: 1: list of categories. */
+				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'some-like-it-neat' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+			}
+			/* translators: used between list items, there is a space after the comma */
+			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'some-like-it-neat' ) );
+			if ( $tags_list ) {
+				/* translators: 1: list of tags. */
+				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'some-like-it-neat' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+			}
+		}
+		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+			echo '<span class="comments-link">';
+			comments_popup_link(
+				sprintf(
+					wp_kses(
+					/* translators: %s: post title */
+						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', '_s' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				)
+			);
+			echo '</span>';
+		}
+		edit_post_link(
+			sprintf(
+				wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'some-like-it-neat' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			),
+			'<span class="edit-link">',
+			'</span>'
+		);
+	}
+endif;
+
+if ( ! function_exists( 'some_like_it_neat_post_format_footer' ) ) :
+	/**
+	 * Prints HTML with meta information for the categories, tags and comments.
+	 */
+    function some_like_it_neat_post_format_footer() {
+        // Hide category and tag text for pages.
+        if ( 'post' === get_post_type() ) {
+            /* translators: used between list items, there is a space after the comma */
+            $categories_list = get_the_category_list( esc_html__( ', ', 'some-like-it-neat' ) );
+            if ( $categories_list && _s_categorized_blog() ) {
+                /* translators: 1: list of categories. */
+                printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'some-like-it-neat' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+            }
+            /* translators: used between list items, there is a space after the comma */
+            $tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'some-like-it-neat' ) );
+            if ( $tags_list ) {
+                /* translators: 1: list of tags. */
+                printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'some-like-it-neat' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+            }
+        }
+    }
+endif;
+
+/**
+ * Add Headroom Script if Offcanvas option is selected
+ */
+if ('offcanvas' === get_theme_mod('some-like-it-neat_nav_style') ) {
+
+    function some_like_it_neat_add_headroomjs()
+    { ?>
+        <script type="text/javascript">
+            (function() {
+                var header = document.querySelector("header#masthead");
+
+                if(window.location.hash) {
+                    header.classList.add("headroom--unpinned");
+                }
+
+                var headroom = new Headroom(header, {
+                    tolerance: {
+                        down : 10,
+                        up : 20
+                    },
+                    offset : 0
+                });
+                headroom.init();
+
+            }());
+        </script>
+    <?php }
+    //Include Headroom
+    add_action( 'wp_footer', 'some_like_it_neat_add_headroomjs' );
+
+}
