0f133bbc5db524e00594539bb5102fa697ad512b
Alex Vasquez <alexjvas@gmail.com>
2017-06-21 23:12:07 -0700

    make post nav conditional

--- library/extras.php
+++ library/extras.php
@@ -11,23 +11,28 @@
 /**
  * Add Singular Post Template Navigation
  */
-if (! function_exists('some_like_it_neat_post_navigation') ) :
+if ('no' === get_theme_mod('some-like-it-neat_hide_post_navigation') )
+{
+	if (! function_exists('some_like_it_neat_post_navigation') ) :
 
-    function some_like_it_neat_post_navigation() 
-    {
-        if (is_singular() && !is_page_template('page-templates/template-landing-page.php')  ) {
-            echo get_the_post_navigation(
-                array(
-                    'prev_text'    => __('&larr; %title', 'some-like-it-neat'),
-                    'next_text'    => __('%title &rarr;', 'some-like-it-neat'),
-                    'screen_reader_text' => __('Page navigation', 'some-like-it-neat')
-                )
-            );
-        }
-    }
+		function some_like_it_neat_post_navigation()
+		{
+			if (is_singular() && !is_page_template('page-templates/template-landing-page.php')  ) {
+				echo get_the_post_navigation(
+					array(
+						'prev_text'    => __('&larr; %title', 'some-like-it-neat'),
+						'next_text'    => __('%title &rarr;', 'some-like-it-neat'),
+						'screen_reader_text' => __('Page navigation', 'some-like-it-neat')
+					)
+				);
+			}
+		}
 
-endif;
-add_action('tha_entry_after', 'some_like_it_neat_post_navigation');
+	endif;
+	add_action('tha_entry_after', 'some_like_it_neat_post_navigation');
+}
+
+
 
 if ( ! function_exists( 'some_like_it_neat_entry_footer' ) ) :
 	/**
