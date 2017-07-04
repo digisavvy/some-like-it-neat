42c7157b8021be1e9f5f67789fab56f42d029bd1
Alex Vasquez <alexjvas@gmail.com>
2017-07-03 23:25:31 -0700

    format code via phpcbf and mass crying!

--- library/assets.php
+++ library/assets.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * some_like_it_neat functions and definitions
+ * Some_like_it_neat functions and definitions
  *
  * @package Some_Like_It_Neat
  * @author  Alex Vasquez <alex@digisavvy.com>
@@ -11,123 +11,132 @@
 /**
  * Enqueue scripts.
  */
-if (! function_exists('some_like_it_neat_scripts') ) :
+if ( ! function_exists( 'some_like_it_neat_scripts' ) ) :
+	/**
+	 * Sets up theme script and style dependencies
+	 *
+	 * Note that this function is hooked into the wp_enqueue_scripts hook, which
+	 * runs after the init hook.
+	 */
+	function some_like_it_neat_scripts() {
 
-    function some_like_it_neat_scripts() 
-    {
+		// Vendor Scripts.
+		wp_register_script( 'modernizr-js', get_theme_file_uri( '/assets/js/vendor/modernizr/modernizr.js' ), array( 'jquery' ), '2.8.2', false );
+		wp_enqueue_script( 'modernizr-js' );
 
-        // Vendor Scripts
-        wp_register_script('modernizr-js', get_theme_file_uri('/assets/js/vendor/modernizr/modernizr.js'), array( 'jquery' ), '2.8.2', false);
-        wp_enqueue_script('modernizr-js');
+		wp_register_script( 'selectivizr-js', get_theme_file_uri( '/assets/js/vendor/selectivizr/selectivizr.js' ), array( 'jquery' ), '1.0.2b', false );
+		wp_enqueue_script( 'selectivizr-js' );
+		wp_script_add_data( 'selectivizr-js', 'conditional', '(gte IE 6)&(lte IE 8)' );
 
-        wp_register_script('selectivizr-js', get_theme_file_uri('/assets/js/vendor/selectivizr/selectivizr.js'), array( 'jquery' ), '1.0.2b', false);
-        wp_enqueue_script('selectivizr-js');
-        wp_script_add_data('selectivizr-js', 'conditional', '(gte IE 6)&(lte IE 8)');
+		if ( 'flexnav' === get_theme_mod( 'some-like-it-neat_nav_style' ) && ! is_page_template( 'page-templates/template-landing-page.php' ) ) {
+			// Flexnav Style.
+			wp_enqueue_style( 'flexnav-css', get_theme_file_uri( '/assets/css/vendor/flexnav.css' ) );
 
-        if ( 'flexnav' === get_theme_mod('some-like-it-neat_nav_style' ) && ! is_page_template( 'page-templates/template-landing-page.php' ) ) {
-            // Flexnav Style
-            wp_enqueue_style('flexnav-css', get_theme_file_uri('/assets/css/vendor/flexnav.css'));
+			// Flexnav Scripts.
+			wp_register_script( 'flexnav-js', get_theme_file_uri( '/assets/js/vendor/flexnav/jquery.flexnav.js' ), array( 'jquery' ), '1.3.3', true );
+			wp_enqueue_script( 'flexnav-js' );
 
-            // Flexnav Scripts
-            wp_register_script('flexnav-js', get_theme_file_uri('/assets/js/vendor/flexnav/jquery.flexnav.js'), array( 'jquery' ), '1.3.3', true);
-            wp_enqueue_script('flexnav-js');
+			wp_register_script( 'hoverintent-js', get_theme_file_uri( '/assets/js/vendor/hoverintent/jquery.hoverIntent.js' ), array( 'jquery' ), '1.0.0', true );
+			wp_enqueue_script( 'hoverintent-js' );
+		} elseif ( 'offcanvas' === get_theme_mod( 'some-like-it-neat_nav_style' ) && ! is_page_template( 'page-templates/template-landing-page.php' ) ) {
+			// Offcanvas Style.
+			wp_enqueue_style( 'offcanvas', get_theme_file_uri( '/assets/css/layouts/navigation-offcanvas.css' ) );
 
-            wp_register_script('hoverintent-js', get_theme_file_uri('/assets/js/vendor/hoverintent/jquery.hoverIntent.js'), array( 'jquery' ), '1.0.0', true);
-            wp_enqueue_script('hoverintent-js');
-        } elseif ( 'offcanvas' === get_theme_mod('some-like-it-neat_nav_style' ) && ! is_page_template( 'page-templates/template-landing-page.php' ) ) {
-            // Offcanvas Style
-            wp_enqueue_style('offcanvas', get_theme_file_uri('/assets/css/layouts/navigation-offcanvas.css'));
+			// Offcanvas Nav Script.
+			wp_enqueue_script( 'some-like-it-neat_custom', get_template_directory_uri() . '/assets/js/vendor/custom-offcanvas.js', array( 'jquery' ), '1.0', true );
+			wp_enqueue_script( 'some-like-it-neat_custom' );
 
-            // Offcanvas Nav Script
-            wp_enqueue_script('some-like-it-neat_custom', get_template_directory_uri() . '/assets/js/vendor/custom-offcanvas.js', array('jquery'), '1.0', true);
-            wp_enqueue_script('some-like-it-neat_custom');
+			// Headroom Style.
+			wp_enqueue_style( 'headroom', get_theme_file_uri( '/assets/css/vendor/headroom-min.css' ) );
 
-            // Headroom Style
-            wp_enqueue_style('headroom', get_theme_file_uri('/assets/css/vendor/headroom-min.css'));
+			// Headroom Script.
+			wp_enqueue_script( 'some-like-it-neat_headroom', get_template_directory_uri() . '/assets/js/vendor/headroom/headroom-min.js', array( 'jquery' ), '0.9.3', false );
+			wp_enqueue_script( 'some-like-it-neat_headroom' );
+		}
 
-            // Headroom Script
-            wp_enqueue_script('some-like-it-neat_headroom', get_template_directory_uri() . '/assets/js/vendor/headroom/headroom-min.js', array('jquery'), '0.9.3', false);
-            wp_enqueue_script('some-like-it-neat_headroom');
-        } else {
-        	// Do Nothing
-        }
+		// Dashicons.
+		wp_enqueue_style( 'dashicons' );
 
-        // Dashicons
-        wp_enqueue_style('dashicons');
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+			wp_enqueue_script( 'comment-reply' );
+		}
 
-        if (is_singular() && comments_open() && get_option('thread_comments') ) {
-            wp_enqueue_script('comment-reply');
-        }
+		/**
+		 * Concatenate Scripts. Checks the directory below for js files. If there are js files they will be concatenated and minified in either
+		 * development.js or production.js. NOTE - You will have to stop and restart gulp. Also, these scripts run on all pages. Make sure
+		 * your scripts actually need to run on all pages before concatenating.
+		 */
+		$directory = get_template_directory() . '/assets/js/app/';
+		$files = glob( $directory . '*.js' );
+		if ( false !== $files ) {
+			$filecount = count( $files );
 
-        /**
-         * Concatenate Scripts. Checks the directory below for js files. If there are js files they will be concatenated and minified in either
-         * development.js or production.js. NOTE - You will have to stop and restart gulp. Also, these scripts run on all pages. Make sure
-         * your scripts actually need to run on all pages before concatenating.
-         */
-        $directory = get_template_directory() . '/assets/js/app/';
-        $files = glob($directory . '*.js');
-        if ($files !== false ) {
-            $filecount = count($files);
+			if ( 0 === ! $filecount ) {
+				if ( SCRIPT_DEBUG || WP_DEBUG ) :
+					// Concatonated Scripts.
+					wp_enqueue_script( 'some_like_it_neat-js', get_theme_file_uri( '/assets/js/development.js' ), array( 'jquery' ), '1.0.0', false );
+				else :
+					// Concatonated Scripts.
+					wp_enqueue_script( 'some_like_it_neat-js', get_theme_file_uri( '/assets/js/production-min.js' ), array( 'jquery' ), '1.0.0', false );
+				endif;
+			}
+		}
+	}
+	add_action( 'wp_enqueue_scripts', 'some_like_it_neat_scripts' );
 
-            if (! $filecount == 0 ) {
-                if (SCRIPT_DEBUG || WP_DEBUG ) :
-                    // Concatonated Scripts
-                    wp_enqueue_script('some_like_it_neat-js', get_theme_file_uri('/assets/js/development.js'), array( 'jquery' ), '1.0.0', false);
-                else :
-                    // Concatonated Scripts
-                    wp_enqueue_script('some_like_it_neat-js', get_theme_file_uri('/assets/js/production-min.js'), array( 'jquery' ), '1.0.0', false);
-                endif;
-            }
-        }
-    }
-    add_action('wp_enqueue_scripts', 'some_like_it_neat_scripts');
-
-endif; // Enqueue scripts
+endif; // Enqueue scripts.
 
 /**
  * Enqueue styles.
  */
-if (! function_exists('some_like_it_neat_styles') ) :
+if ( ! function_exists( 'some_like_it_neat_styles' ) ) :
+	/**
+	 * Calls theme styles.
+	 *
+	 * Note that this function should conditionally load RTL or LTR styles based on the is_rtl() function's ability
+	 * to detect the user's browser locale.
+	 *
+	 * @link https://codex.wordpress.org/Function_Reference/is_rtl
+	 */
+	function some_like_it_neat_styles() {
 
-    function some_like_it_neat_styles() 
-    {
-    	if ( !is_rtl() ) {
+		if ( ! is_rtl() ) {
 
-			if (SCRIPT_DEBUG || WP_DEBUG ) :
-			    wp_register_style(
-			        'some_like_it_neat-style', // handle name
-			        get_parent_theme_file_uri('/assets/css/style.css'), '', '1.2', 'screen'
-			    );
-			    wp_enqueue_style('some_like_it_neat-style');
+			if ( SCRIPT_DEBUG || WP_DEBUG ) :
+				wp_register_style(
+					'some_like_it_neat-style', // handle name.
+					get_parent_theme_file_uri( '/assets/css/style.css' ), '', '1.2', 'screen'
+				);
+				wp_enqueue_style( 'some_like_it_neat-style' );
 
 			else :
-			    wp_register_style(
-			        'some_like_it_neat-style', // handle name
-			        get_parent_theme_file_uri('/assets/css/style-min.css'), '', '1.2', 'screen'
-			    );
-			    wp_enqueue_style('some_like_it_neat-style');
+				wp_register_style(
+					'some_like_it_neat-style', // handle name.
+					get_parent_theme_file_uri( '/assets/css/style-min.css' ), '', '1.2', 'screen'
+				);
+				wp_enqueue_style( 'some_like_it_neat-style' );
 			endif;
 
-	    } else {
+		} else {
 
-		    if (SCRIPT_DEBUG || WP_DEBUG ) :
-			    wp_register_style(
-				    'some_like_it_neat-style', // handle name
-				    get_parent_theme_file_uri('/assets/css/rtl.css'), '', '1.2', 'screen'
-			    );
-			    wp_enqueue_style('some_like_it_neat-style');
+			if ( SCRIPT_DEBUG || WP_DEBUG ) :
+				wp_register_style(
+					'some_like_it_neat-style', // handle name.
+					get_parent_theme_file_uri( '/assets/css/rtl.css' ), '', '1.2', 'screen'
+				);
+				wp_enqueue_style( 'some_like_it_neat-style' );
 
-		    else :
-			    wp_register_style(
-				    'some_like_it_neat-style', // handle name
-				    get_parent_theme_file_uri('/assets/css/rtl-min.css'), '', '1.2', 'screen'
-			    );
-			    wp_enqueue_style('some_like_it_neat-style');
-		    endif;
+			else :
+				wp_register_style(
+					'some_like_it_neat-style', // handle name.
+					get_parent_theme_file_uri( '/assets/css/rtl-min.css' ), '', '1.2', 'screen'
+				);
+				wp_enqueue_style( 'some_like_it_neat-style' );
+			endif;
 
-	    }
+		}
 
-    }
-    add_action('wp_enqueue_scripts', 'some_like_it_neat_styles');
+	}
+	add_action( 'wp_enqueue_scripts', 'some_like_it_neat_styles' );
 
-endif; // Enqueue styles
+endif; // Enqueue styles.
--- library/vendors/customizer/customizer.php
+++ library/vendors/customizer/customizer.php
@@ -40,6 +40,29 @@ function some_like_it_neat_sanitize_checkbox( $input )
 }
 
 /**
+ * HEX Color sanitization callback example.
+ *
+ * - Sanitization: hex_color
+ * - Control: text, WP_Customize_Color_Control
+ *
+ * Note: sanitize_hex_color_no_hash() can also be used here, depending on whether
+ * or not the hash prefix should be stored/retrieved with the hex color value.
+ *
+ * @see sanitize_hex_color() https://developer.wordpress.org/reference/functions/sanitize_hex_color/
+ * @link sanitize_hex_color_no_hash() https://developer.wordpress.org/reference/functions/sanitize_hex_color_no_hash/
+ *
+ * @param string               $hex_color HEX color to sanitize.
+ * @param WP_Customize_Setting $setting   Setting instance.
+ * @return string The sanitized hex color if not null; otherwise, the setting default.
+ */
+function some_like_it_neat_sanitize_hex_color( $hex_color, $setting ) {
+	// Sanitize $input as a hex value without the hash prefix.
+	$hex_color = sanitize_hex_color( $hex_color );
+	// If $input is a valid hex value, return it; otherwise, return the default.
+	return ( ! null( $hex_color ) ? $hex_color : $setting->default );
+}
+
+/**
  * Customizer Some Like it Neat Additions
  */
 
