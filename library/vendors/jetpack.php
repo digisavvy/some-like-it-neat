5e3d1ec1bfc9c45985e70f2b2a0d4e188414bb7f
Alex Vasquez <alexjvas@gmail.com>
2017-07-07 09:06:37 -0700

    update jetpackinfinite scroll

--- library/vendors/jetpack.php
+++ library/vendors/jetpack.php
@@ -10,10 +10,27 @@
  * Add theme support for Infinite Scroll.
  * See: http://jetpack.me/support/infinite-scroll/
  */
-function some_like_it_neat_jetpack_setup() {
-	add_theme_support( 'infinite-scroll', array(
-		'container' => 'main',
-		'footer'    => 'page',
-	) );
+if ( 'yes' === get_theme_mod( 'some-like-it-neat_infinite_scroll_support' ) ) {
+	$scroll_type = get_theme_mod( 'some-like-it-neat_infinite_scroll_type' );
+	add_theme_support(
+		'infinite-scroll', array(
+			'type'           => $scroll_type,
+			'footer'         => 'page',
+			'footer_widgets' => false,
+			'container'      => 'main',
+			'wrapper'        => true,
+			'render'         => 'some_like_it_neat_infinite_scroll_render',
+			'posts_per_page' => false,
+		)
+	);
 }
-add_action( 'after_setup_theme', 'some_like_it_neat_jetpack_setup' );
+
+/**
+ * Grabs posts while posts exists for infinite loop.
+ */
+function some_like_it_neat_infinite_scroll_render() {
+	if ( have_posts() ) : while ( have_posts() ) : the_post();
+		get_template_part( 'page-templates/template-parts/content', get_post_format() );
+	endwhile;
+	endif;
+}
