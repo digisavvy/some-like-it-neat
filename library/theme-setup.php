136f1769fd774e6183cfc3764fe3d9e7b65bc3f0
Alex Vasquez <alexjvas@gmail.com>
2018-01-30 07:21:09 -0800

    update theme setup for carbon fields

--- library/theme-setup.php
+++ library/theme-setup.php
@@ -11,22 +11,10 @@
 /**
  * Including Carbon Fields (https://github.com/htmlburger/carbon-fields).
  */
-use Carbon_Fields\Container;
-use Carbon_Fields\Field;
-
-add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' );
-function crb_attach_theme_options() {
-	Container::make( 'theme_options', __( 'Theme Options', 'crb' ) )
-		->add_fields( array(
-			Field::make( 'text', 'crb_text', 'Text Field' ),
-		) );
-}
-
 add_action( 'after_setup_theme', 'crb_load' );
 function crb_load() {
 	require_once( get_template_directory() . '/vendor/autoload.php' );
 	\Carbon_Fields\Carbon_Fields::boot();
-	include get_template_directory() . '/library/vendors/meta.php';
 }
 
 if ( ! function_exists( 'some_like_it_neat_setup' ) ) :
