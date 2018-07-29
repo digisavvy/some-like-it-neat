67831be25ea19bf2f36be8a994af18eca6e13ddc
Alex Vasquez <alexjvas@gmail.com>
2018-07-29 16:49:45 -0700

    Add condition to display entry meta on posts
    Adds entry meta on page templates that are assigned to single post content.

--- page-templates/template-parts/content-page.php
+++ page-templates/template-parts/content-page.php
@@ -19,6 +19,12 @@
 	<header class="entry-header">
 
 		<h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1>
+		
+		<?php if ( 'post' === get_post_type() ) : ?>
+
+			<?php get_template_part( 'page-templates/template-parts/content', 'meta' ); ?>
+
+		<?php endif; ?>
 
 	</header><!-- .entry-header -->
 
